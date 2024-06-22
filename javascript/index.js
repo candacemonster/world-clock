c4d52bafc55785e29dbd774ada606e0ded166830
Candace Woodbury <112183197+candacemonster@users.noreply.github.com>
2024-06-22 16:58:27 -0700

    added the functionality for select city
    added necessary html and js for select city updates for times.

--- javascript/index.js
+++ javascript/index.js
@@ -1,26 +1,79 @@
-function updateTime() {
-//Las Vegas 
-let lasVegasElement = document.querySelector("#las-vegas");
-let lasVegasDateElement = lasVegasElement.querySelector(".date");
-let lasVegasTimeElement = lasVegasElement.querySelector(".time");
-let lasVegasTime = moment().tz("America/Los_Angeles");
-
-
-lasVegasDateElement.innerHTML = lasVegasTime.format("dddd, MMMM Do YYYY	");
-lasVegasTimeElement.innerHTML = lasVegasTime.format("h:mm:ss [<small>]A[</small>]"
-);
-
-//Zulu
-let zuluElement = document.querySelector("#zulu");
-let zuluDateElement = zuluElement.querySelector(".date");
-let zuluTimeElement = zuluElement.querySelector(".time");
-let zuluTime = moment().tz("Zulu");
-
-
-zuluDateElement.innerHTML = zuluTime.format("dddd, MMMM Do YYYY	");
-zuluTimeElement.innerHTML = zuluTime.format("h:mm:ss [<small>]A[</small>]"
-);
-}
-
-updateTime();
-setInterval(updateTime, 1);
+const globalTimezones = [
+    "America/New_York",
+    "America/Los_Angeles",
+    "America/Chicago",
+    "America/Toronto",
+    "Europe/London",
+    "Europe/Paris",
+    "Europe/Berlin",
+    "Europe/Moscow",
+    "Asia/Dubai",
+    "Asia/Singapore",
+    "Asia/Tokyo",
+    "Asia/Shanghai",
+    "Australia/Sydney",
+    "Pacific/Auckland",
+  ];
+  
+  let selectedCity = null;
+  
+  function updateTime() {
+    // Zulu Time
+    let zuluTime = moment().utc();
+    updateTimeDisplay("zulu", "Zulu Time (UTC+00:00)", zuluTime);
+  
+    // Selected City or Opposite of Zulu Time
+    if (selectedCity) {
+      let cityTime = moment().tz(selectedCity);
+      let cityName = selectedCity.split("/")[1].replace("_", " ");
+      let utcOffset = cityTime.format("Z");
+      updateTimeDisplay("opposite", `${cityName} (UTC${utcOffset})`, cityTime);
+    } else {
+      // Opposite of Zulu Time (UTC-12:00)
+      let oppositeTime = moment().tz("Etc/GMT+12");
+      updateTimeDisplay("opposite", "Opposite Zulu (UTC-12:00)", oppositeTime);
+    }
+  }
+  
+  function updateTimeDisplay(elementId, title, time) {
+    let element = document.getElementById(elementId);
+    element.innerHTML = `
+      <div class="city">
+        <div>
+          <h2>${title}</h2>
+          <div class="date">${time.format("dddd, MMMM Do YYYY")}</div>
+        </div>
+        <div class="time">
+          ${time.format("h:mm:ss")}
+          <small>${time.format("A")}</small>
+        </div>
+      </div>
+    `;
+  }
+  
+  function populateSelect() {
+    const select = document.getElementById('city');
+    globalTimezones.forEach(timezone => {
+      const option = document.createElement('option');
+      option.value = timezone;
+      option.textContent = timezone.split('/')[1].replace('_', ' ');
+      select.appendChild(option);
+    });
+  }
+  
+  function updateCity(event) {
+    selectedCity = event.target.value;
+    if (!selectedCity) {
+      selectedCity = null; 
+    }
+    updateTime(); 
+  
+  document.addEventListener('DOMContentLoaded', (event) => {
+    populateSelect();
+    
+    const select = document.getElementById('city');
+    select.addEventListener('change', updateCity);
+  
+    updateTime();
+    setInterval(updateTime, 1000);
+  });
